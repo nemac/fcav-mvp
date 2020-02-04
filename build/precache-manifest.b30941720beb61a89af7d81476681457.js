09c01b67aa20607af269bd890a055273fb0c7df9
Matthew Geiger <mgeiger@unca.edu>
2020-02-04 15:26:52 -0500

    Build

---
+++ build/precache-manifest.b30941720beb61a89af7d81476681457.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "148f6f641da97a85edfd4082362b7f12",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1537504973c122af6dea",
+    "url": "/static/js/2.b78d7d53.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.b78d7d53.chunk.js.LICENSE"
+  },
+  {
+    "revision": "524c5ed31f7a1e197e82",
+    "url": "/static/js/main.36e6d122.chunk.js"
+  },
+  {
+    "revision": "c57601fd55615e52212c",
+    "url": "/static/js/runtime-main.060919c2.js"
+  },
+  {
+    "revision": "c58a088df95d4e68c3bbb3c04e252709",
+    "url": "/static/media/nemac_trans_500.c58a088d.png"
+  }
+]);
